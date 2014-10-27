5f2d2efadeeaaccffa8449a20f3a3bcbc23bb754
Po-Chih Lin <pcl322@nyu.edu>
2014-10-27 16:03:33 -0400

    Add more comments

--- pcl322/interval/intervalExceptions.py
+++ pcl322/interval/intervalExceptions.py
@@ -1,9 +1,11 @@
+#Invalid interval initialization exception
 class InitErr(Exception):
         def __init__(self, message):
                 self.message = message
         def __str__(self):
                 return self.message
 
+#Merging two non-overlapping intervals exception
 class MergeErr(Exception):
         def __str__(self):
                 return "Can not merge."
